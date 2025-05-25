eb5dcc6a043c22ae51413c01b1e71428c1d98bee
LCDsuperobot129 <ckev78@gmail.com>
2025-05-24 22:00:34 -0700

    Update InputTPRoundInitiatorServer.lua

--- ServerScriptService/InputTPRoundInitiatorServer.lua
+++ ServerScriptService/InputTPRoundInitiatorServer.lua
@@ -10,10 +10,12 @@ for _, part in CollectionService:GetTagged("InputTP") do
 			local hum = char.Humanoid
 			local output = part.Parent.Output
 			
-			if player:WaitForChild("MaxObbyLevel").Value >= part:WaitForChild("Value").Value then
-				hit.Parent.HumanoidRootPart.Position = output.Position + Vector3.new(0, 3, 0)
-			else
-				player.PlayerGui:WaitForChild("Sounds"):WaitForChild("NoPass"):Play()
+			if part:FindFirstChild("Value") then
+				if player:WaitForChild("MaxObbyLevel").Value >= part:WaitForChild("Value").Value then
+					hit.Parent.HumanoidRootPart.Position = output.Position + Vector3.new(0, 3, 0)
+				else
+					player.PlayerGui:WaitForChild("Sounds"):WaitForChild("NoPass"):Play()
+				end
 			end
 
 			--[[ local speed = hum.WalkSpeed  -- studs per sec, currently set to player humanoid's walkspeed
